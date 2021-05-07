fa87506587263cb49eb908c8306437a14f373427
Baisleac <staky@ryanweb.id.au>
2021-05-07 10:14:56 +1000

    Starting the process for a dynamic variable to pass from index.js to index.pug

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const pug = require('pug');
 
 const PORT = process.env.PORT || 1234
 
+const data = { jobs: ['accountant', 'electrician', 'mechanic', 'teacher'] }
+
 const server = createServer((request,response) => {
-	return response.end(pug.renderFile('views/index.pug'))
+	return response.end(pug.renderFile('views/index.pug', data))
 })
 
 server.listen(PORT, () => {
