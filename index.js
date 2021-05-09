6f01e493f84d887a8475041a007d0e58ea4a58ba
Baisleac <staky@ryanweb.id.au>
2021-05-09 11:17:42 +1000

    New page loads via link

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 require('dotenv').config()
 const axios = require('axios')
 const { createServer } = require('http')
+const url = require('url')
 const pug = require('pug')
 
 const PORT = process.env.PORT || 1234
 
+//Leave this outside so the server (thus pug flies) can access it
 const data = {}
 
 const router = [
-  {path: '/', template: 'views/index.pug'},
-  {path: '/other.html', template: 'views/other.pug'}
+  { path: '/', template: 'views/index.pug', controller: showAll },
+  { path: '/other.html', template: 'views/other.pug', controller: showJob }
 ]
 
-axios.get('http://api.dataatwork.org/v1/jobs').then(res => {
-  data.jobs = res.data;
-}).catch((error) => console.log('API error', error))
+const showAll = () => {
+return {/* object */}
+}
 
 //event listener - waiting for the page to be loaded the first time
 //This is a Node.js event listener
 //Using a method instead of if statement because if more routes are added, this will be cleaner
-const server = createServer((request,  response) => {
-  console.log(request.url);
-  const templateFile = router.find(route => request.url === route.path)
+const server = createServer((request, response) => {
+  const inboundURL = url.parse(request.url, {parseQueryString: true})
+  
+  //Find a matching route for the inbound URL
+  const templateFile = router.find(route => inboundURL.pathname === route.path)
   if (templateFile) {
-    return (response.end(pug.renderFile(templateFile.template, data)))
+    //Get data from jobs API
+    axios.get('http://api.dataatwork.org/v1/jobs').then(res => {
+      data.jobs = res.data;
+      return (response.end(pug.renderFile(templateFile.template, data)))
+    }).catch((error) => console.log('API error', error))
   } else {
-	return response.end(pug.renderFile('views/404.pug', data));
+    return response.end(pug.renderFile('views/404.pug', data));
   }
 })
 
