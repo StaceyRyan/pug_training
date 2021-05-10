417629dc15d851a1ce01722e13d48ed8debf9189
Baisleac <staky@ryanweb.id.au>
2021-05-10 22:02:01 +1000

    Modernised url parser

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const axios = require('axios')
 const { createServer } = require('http')
-const url = require('url')
 const pug = require('pug')
 
 const PORT = process.env.PORT || 1234
@@ -12,20 +11,28 @@ const data = {}
 //Controller functions
 //mandatory argument goes first, optional later
 const showAll = (jobs, url) => {
-  return {jobs: jobs}
+  console.log('jobs', jobs.slice(0, 3))
+  let pageNum = 0
+  if (!url.query.page) {
+    pageNum = 1
+  } else {
+    pageNum = url.searchParams.page
+  }
+  let endIndex = pageNum * 3
+  let startIndex = endIndex - 3
+  return { jobs: jobs.slice(startIndex, endIndex), page: url.searchParams.page }
 }
 const showJob = (jobs, url) => {
   // console.log(JSON.stringify(jobs))
-  const jobDetail = jobs.find(job => job.uuid === url.query.id)
+  const jobDetail = jobs.find(job => job.uuid === url.searchParams.get('id'))
   // console.log(`showJob ${jobDetail.title}`)
   if (jobDetail) {
-    return {job: jobDetail} 
+    return { job: jobDetail }
   }
-  else return { 
-    job: {title: `Error - job ${url.query.id} not found`}
+  else return {
+    job: { title: `Error - job ${url.searchParams.get('id')} not found` }
   }
 }
-//test
 
 const router = [
   { path: '/', template: 'views/index.pug', controller: showAll },
@@ -36,7 +43,8 @@ const router = [
 //This is a Node.js event listener
 //Using a method instead of if statement because if more routes are added, this will be cleaner
 const server = createServer((request, response) => {
-  const inboundURL = url.parse(request.url, {parseQueryString: true})
+  // const inboundURL = url.parse(request.url, { parseQueryString: true })
+  const inboundURL = new URL(`http://${request.headers.host}${request.url}`)
 
   //Find a matching route for the inbound URL
   const templateFile = router.find(route => inboundURL.pathname === route.path)
@@ -44,7 +52,7 @@ const server = createServer((request, response) => {
     //Get data from jobs API
     axios.get('http://api.dataatwork.org/v1/jobs').then(res => {
       data.jobs = res.data;
-      return (response.end(pug.renderFile(templateFile.template, 
+      return (response.end(pug.renderFile(templateFile.template,
         templateFile.controller(data.jobs, inboundURL))))
     }).catch((error) => console.log('API error', error))
   } else {
