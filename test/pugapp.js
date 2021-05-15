83e960af0374dc843cb867a8333dc10581aaf614
Baisleac <staky@ryanweb.id.au>
2021-05-15 11:58:17 +1000

    Testing not working with mocha and chai

--- test/pugapp.js
+++ test/pugapp.js
@@ -1,13 +1,22 @@
-let expect = require("chai").expect;
-let apiCall = require("../index")
+const expect = require("chai").expect
+const request = require('superagent')
+const apiCall = require("../index")
+
 
 //details that describe elements of the app
 //describe('test title', function)
 
 //Test Server
-describe("API call to jobs", function() {
-    let serverTest = 'http://localhost:3000'
-
+describe("API call to jobs", () => {
+  beforeEach(() => {
+    nock('http://localhost:3000').reply(200, response)
+  })
+  it("should return status 200", async () => {
+    let res = await chai.request(apiCall).
+    // let serverTest = 
+    expect(response.statusCode).to.deep.equal(200)
+  })
+/*
     //expectations go into the body of the it function
     it("Returns status 200", function() {
         request(serverTest, function(error, response, body) {
@@ -16,4 +25,5 @@ describe("API call to jobs", function() {
           done()
         })
     })
+    */
 })
---
+++ test/response.js
@@ -0,0 +1,8 @@
+module.exports = {
+	jobs: [
+    	"job1",
+    	"job2",
+    	"job3"
+	],
+	status: "200"
+}
